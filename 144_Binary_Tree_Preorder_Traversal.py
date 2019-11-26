bcc61195a27fc4e22355bc764f2d319bca3d4107
soshi-matsuo <msoshi1202@gmail.com>
2019-11-26 19:38:44 +0900

    learn Queue and Binary Tree

---
+++ 144_Binary_Tree_Preorder_Traversal.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        stack = [root]
+        result = []
+        while stack:
+            node = stack.pop()
+            if node:
+                result.append(node.val)
+                stack.append(node.right)
+                stack.append(node.left)
+        return result
---
+++ 94_Binary_Tree_Inorder_Traversal.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        stack = []
+        result = []
+        current = root
+        while current or stack:
+            while current:
+                stack.append(current)
+                current = current.left
+            current = stack.pop()
+            result.append(current.val)
+            current = current.right
+        return result
