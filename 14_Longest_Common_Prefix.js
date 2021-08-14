e4121b631615fd20d2ef493aa3a5c4a77653e187
soshi-matsuo <msoshi1202@gmail.com>
2021-08-14 12:29:57 +0900

    Solve 14.Longest Common Prefix with js

--- 14_Longest_Common_Prefix.js
+++ 14_Longest_Common_Prefix.js
@@ -1,8 +1,20 @@
-leetcode: 14.Longest Common Prefix
-・入力→文字列を要素に持つ配列strs
-・出力→strsの各要素の中で、最も長い共通の文字列
-・制約
-・共通部分がなければ空文字を返す
-・1 <= strs.length <= 200
-・0 <= strs[i].length <= 200
-・strs[i]は小文字のみで構成される
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    if (strs.length === 1) return strs[0];
+    else {
+        const first = strs.shift();
+        const rest = longestCommonPrefix(strs);
+        // firstとrestの共通部分算出
+        let common = '';
+        for (let i = 0; i < rest.length; i++) {
+            if (first[i] === rest[i]) common += rest[i];
+            else break;
+        }
+        return common;
+    }
+};
+
+
