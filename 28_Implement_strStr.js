09f9729ca7119d830124f25ed01f2fff00b6b8c3
soshi-matsuo <msoshi1202@gmail.com>
2021-08-19 23:46:46 +0900

    Start 28.Implement strStr

---
+++ 28_Implement_strStr.js
@@ -0,0 +1,6 @@
+・入力→検索対象の文字列haystackと、haystackの部分文字列であるneedle
+・出力→haystackの中で、needleが最初に登場するidx
+・制約
+・needleがhaystack中に存在しない場合は-1、needleが空文字の場合は0を返す
+・いずれも小文字のアルファベットで構成される
+・0 <= haystack.length, needle.length <= 5 * 10^4
