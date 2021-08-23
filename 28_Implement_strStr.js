f47054951ed142e30db7c03c508249084c8fba51
soshi-matsuo <msoshi1202@gmail.com>
2021-08-23 13:30:12 +0900

    Solve 28.Implement strStr()

--- 28_Implement_strStr.js
+++ 28_Implement_strStr.js
@@ -1,6 +1,15 @@
-・入力→検索対象の文字列haystackと、haystackの部分文字列であるneedle
-・出力→haystackの中で、needleが最初に登場するidx
-・制約
-・needleがhaystack中に存在しない場合は-1、needleが空文字の場合は0を返す
-・いずれも小文字のアルファベットで構成される
-・0 <= haystack.length, needle.length <= 5 * 10^4
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    if (!needle.length) return 0;
+    for (let i = 0; i <= haystack.length - needle.length; i++) {
+        for (let j = 0; j < needle.length; j++) {
+            if (haystack[i+j] !== needle[j]) break; 
+            if (j === needle.length - 1) return i;
+        }
+    }    
+    return -1;
+};
