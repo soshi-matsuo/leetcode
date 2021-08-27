ca600abc37d5042ac4cc6badd0fe006b50222334
soshi-matsuo <msoshi1202@gmail.com>
2021-08-27 19:37:44 +0900

    Solve 58.Length of Last Word with js

---
+++ 58_Length_of_Last_Word.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    let length = 0;
+    for (let i = s.length - 1; i >= 0; i--) {
+        if (s[i] !== ' ') length++;
+        else if (length > 0) return length;
+    }
+    return length;
+};
