58aea28ff4e3a1c99dd44c6899c1976d5528ecf7
soshi-matsuo <msoshi1202@gmail.com>
2021-09-03 12:53:24 +0900

    Improve 70.Climbing Stairs

--- 70_Climbing_Stairs.js
+++ 70_Climbing_Stairs.js
@@ -2,15 +2,34 @@
  * @param {number} n
  * @return {number}
  */
-const memo = {};
+// const memo = {};
 
+// var climbStairs = function(n) {
+    // basic case
+//    if (n <= 3) return n;
+
+    // recursive case
+//    if (memo[n]) return memo[n];
+//    const steps = climbStairs(n - 1) + climbStairs(n - 2);
+//    memo[n] = steps;
+//    return steps;
+//};
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
 var climbStairs = function(n) {
     // basic case
     if (n <= 3) return n;
-
-    // recursive case
-    if (memo[n]) return memo[n];
-    const steps = climbStairs(n - 1) + climbStairs(n - 2);
-    memo[n] = steps;
-    return steps;
+    
+    const memo = new Array(n+1);
+    memo[0] = 0;
+    memo[1] = 1;
+    memo[2] = 2;
+    memo[3] = 3;
+    for (let i = 4; i <= n; i++) {
+        memo[i] = memo[i-1] + memo[i-2];
+    }
+    return memo[n];
 };
