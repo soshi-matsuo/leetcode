647b8d7819c218c5bfbde48872da0889198e2515
soshi-matsuo <msoshi1202@gmail.com>
2021-09-14 13:38:49 +0900

    Solve 144.Binary Tree Preorder Traversal recursively

--- 144_Binary_Tree_Preorder_Traversal.js
+++ 144_Binary_Tree_Preorder_Traversal.js
@@ -1,8 +1,23 @@
-・入力→二分木のノードを表すTreeNodeのインスタンスroot
-・出力→二分木の各ノードの値を、rootからpreorderでノードを走査した順番で配列に格納して返す
-・制約
-・二分木に含まれるノードの数は0~100個
-・-100 <= node.val <= 100
-・実装
-・preorderなので、root→左の子ノード→右の子ノードの優先順位でDFで走査すればOK
-・単純なDFSの実装と同じなので、node.right, node.leftの順にstackへpushして、node.valをresultへpushするのを繰り返せばいい
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function(root) {
+    const result = [];
+    if (root) dfs(root, result);
+    return result;
+};
+
+const dfs = (node, arr) => {
+    arr.push(node.val);
+    if (node.left) dfs(node.left, arr);
+    if (node.right) dfs(node.right, arr);
+}
